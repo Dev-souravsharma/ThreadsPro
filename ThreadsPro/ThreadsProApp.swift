6010e0096fd81500b0838a2ca1b4716fb63b590b
Sourav Sharma <dev.souravsharma@gmail.com>
2024-08-18 15:32:35 +0530

    Initial Commit

---
+++ ThreadsPro/ThreadsProApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsProApp.swift
+//  ThreadsPro
+//
+//  Created by Sourav Sharma on 18/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
